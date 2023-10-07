c921b75645f7afeb226dca761e5ab4e55ea9a2f8
Peter Njiru <peternjirum@gmail.com>
2023-10-07 00:01:19 -0400

    adds postgress db

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from config import app, Resource, api, jsonify, make_response
-from models import Program
+from .models import Program
+
 
 class AfterSchoolPrograms(Resource):
     def get(self):
         programs = [program.to_dict() for program in Program.query.all()]
         return make_response(jsonify(programs), 200)
+
 
 api.add_resource(AfterSchoolPrograms, '/')
 
