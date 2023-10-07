d474bfc89c0ade5f740b007d99fbf7a23c1acd4d
Peter Njiru <peternjirum@gmail.com>
2023-10-06 22:11:54 -0400

    adds a test dictionary

--- server/app.py
+++ server/app.py
@@ -1,8 +1,14 @@
 from config import app, Resource, api
 
+# test dictionary to replace with acrual db
+programs = {
+    'after school 1': {'name': 'happy kids inc.'},
+    'after school 2': {'name': 'hebrew school'}
+}
+
 class Program(Resource):
     def get(self):
-        return "Habari!"
+        return programs
 
 api.add_resource(Program, '/')
 
