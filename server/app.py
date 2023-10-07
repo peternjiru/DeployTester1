9c4db19c2897b9770fafd84eaa60012be1dbab89
Peter Njiru <peternjirum@gmail.com>
2023-10-06 22:15:54 -0400

    adds another test dictionary

--- server/app.py
+++ server/app.py
@@ -2,8 +2,9 @@ from config import app, Resource, api
 
 # test dictionary to replace with acrual db
 programs = {
-    'after school 1': {'name': 'happy kids inc.'},
-    'after school 2': {'name': 'hebrew school'}
+    'after school 1': {'name': 'happy kids inc.', 'grade':'MS'},
+    'after school 2': {'name': 'hebrew school', 'grade':'7'},
+    'after school 3': {'name': 'happy tots llc', 'grade':'1'}
 }
 
 class Program(Resource):
